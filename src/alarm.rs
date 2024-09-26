fa11d22f0628237b046674f4d140b8c896526d50
ronik-v <andronnikovvv@gmail.com>
2024-09-27 00:23:29 +0300

    update: code refactor [alarm]

--- src/alarm.rs
+++ src/alarm.rs
@@ -8,26 +8,39 @@ use chrono::{Datelike, FixedOffset, Timelike, Utc};
 const HOUR: i32 = 3600;
 static mut SEND_STATE: bool = false;
 
-async fn send_reminder(bot: &Bot, chat_id: ChatId, message_text: &str) {
-    bot.send_message(chat_id, message_text)
-        .parse_mode(ParseMode::Html)
-        .await
-        .unwrap();
+
+pub struct Alarm<'bot, 'message_text> {
+    pub bot: &'bot Bot,
+    pub chat_id: ChatId,
+    pub message_text: &'message_text str,
+    pub time_zone: i32
 }
 
+impl <'bot, 'message_text> Alarm<'bot, 'message_text> {
+    pub fn new(bot: &'bot Bot, chat_id: ChatId,  message_text: &'message_text str, time_zone: i32) -> Self {
+        Self { bot, chat_id, message_text, time_zone }
+    }
 
-pub async fn send(bot: &Bot, chat_id: ChatId, message_text: String, time_zone: i32) {
-    let tz = FixedOffset::east_opt(time_zone * HOUR).expect("Invalid time zone offset");
-    let now = Utc::now().with_timezone(&tz);
-    let last_day_of_month: bool = (now + chrono::Duration::days(1)).month() != now.month();
+    async fn send_reminder(&self) {
+        self.bot.send_message(self.chat_id, self.message_text)
+            .parse_mode(ParseMode::Html)
+            .await
+            .unwrap();
+    }
 
-    unsafe {
-        if last_day_of_month && (now.hour() == 9 || now.hour() == 22) {
-            if !SEND_STATE { send_reminder(bot, chat_id, &message_text).await; }
+    pub async fn send(&self) {
+        let tz = FixedOffset::east_opt(self.time_zone * HOUR).expect("Invalid time zone offset");
+        let now = Utc::now().with_timezone(&tz);
+        let last_day_of_month: bool = (now + chrono::Duration::days(1)).month() != now.month();
 
-            SEND_STATE = true;
-        } else {
-            SEND_STATE = false;
+        unsafe {
+            if last_day_of_month && (now.hour() == 9 || now.hour() == 22) {
+                if !SEND_STATE { self.send_reminder().await; }
+
+                SEND_STATE = true;
+            } else {
+                SEND_STATE = false;
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use teloxide::Bot;
 use teloxide::prelude::Requester;
 use tokio::time::{self, Duration};
 use config::{AppData, app_data};
-use alarm::send;
+use alarm::Alarm;
 
 
 #[tokio::main]
@@ -16,10 +16,11 @@ async fn main() {
 
     let app_data: AppData = app_data();
     let bot = Bot::new(app_data.bot_token);
+    let alarm = Alarm::new(&bot, app_data.chat_id, &app_data.message_text, app_data.time_zone);
     let mut interval = time::interval(Duration::from_secs(60));
 
     loop {
         interval.tick().await;
-        send(&bot, app_data.chat_id, app_data.message_text.clone(), app_data.time_zone).await;
+        alarm.send().await;
     }
 }
